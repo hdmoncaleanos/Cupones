f6cdb4897848934d2035bf7eff68f9f7c352d8b5
hdmoncaleano <hdmoncaleano@htsoft.co>
2020-06-28 23:55:11 -0500

    Mostrar grafo para el primer ambiente

--- test/PoissonGeneratorTest.java
+++ test/PoissonGeneratorTest.java
@@ -1,16 +1,17 @@
-import com.sistema.generadores.GeneradorGeometrica;
-import com.sistema.generadores.GeneradorPoisson;
+import org.apache.commons.math3.distribution.NormalDistribution;
+
+import com.sistema.generadores.GeneradorBinomial;
 import com.sistema.interfaz.generadores.GeneradorLaminas;
 
 
 public class PoissonGeneratorTest {
 	public static void main(String[] args) {
-		GeneradorLaminas generador = new GeneradorGeometrica();
-		generador.setN(2);
+		GeneradorLaminas generador = new GeneradorBinomial();
+		generador.setN(100);
 		Integer min_lamina = null;
 		Integer max_lamina = null;
-		for(int i=0;i<200;i++){
-			Integer nueva_lamina = generador.obtenerLamina();
+		for(int i=0;i<10000;i++){
+			int nueva_lamina = generador.obtenerLamina();
 			if(min_lamina == null){
 				min_lamina = nueva_lamina;
 			}
@@ -28,5 +29,21 @@ public class PoissonGeneratorTest {
 			
 		}
 		System.out.println(max_lamina + " " + min_lamina);
+	}
+	
+	static double obtenerLamina(){
+		NormalDistribution distribucion = new NormalDistribution(1, 1);
+//		double factor = distribucion.probability(distribucion.getMean() - (3 * distribucion.getStandardDeviation()),
+//				distribucion.getMean() + (3 * distribucion.getStandardDeviation()));
+//
+//		double prob = ((distribucion.sample() * factor)  + (distribucion.getStandardDeviation() * 3)) / (distribucion.getStandardDeviation() * 6);
+//
+//		if(this.n != null){
+//			Integer laminilla = (int) Math.floor(prob * this.n);
+////			System.out.println(laminilla);
+//			++laminilla;
+//			return laminilla;
+//		}
+		return Math.floor(distribucion.sample());
 	}
 }
